68fa1ef48b06d10483720ac55ddaa78087c8f06a
Grant Lodge <grant.lodge@outlook.com>
2018-10-18 20:56:56 +0100

    core app version

---
+++ Lamp/EngineCore.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Lamp
+{
+    public class EngineCore : IEngineCore
+    {
+        public void Load()
+        {
+        }
+
+        public async Task<string> QueryAsync(string queryText)
+        {
+
+            return await Task.FromResult("No Results Found");
+        }
+    }
+}
---
+++ Lamp/IEngineCore.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Lamp
+{
+    public interface IEngineCore
+    {
+        void Load();
+        Task<string> QueryAsync(string queryText);
+    }
+}
